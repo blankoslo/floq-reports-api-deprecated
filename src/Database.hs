4e4fd5a70d02801f8f81bca6854784c0198c4520
Erlend Hamberg <eh@blankoslo.no>
2016-06-18 12:17:42 +0200

    Produce Excel-friendly CSV
    - Output a project heading before employee + hours rows
    - Use Windows newlines (CRLF)

--- src/Database.hs
+++ src/Database.hs
@@ -1,45 +1,65 @@
+{-# Language LambdaCase #-}
 {-# Language OverloadedStrings #-}
 {-# Language QuasiQuotes #-}
 
 module Database
   (
-    projects
+    project
+  , projects
   , employeeHours
+  , projectHours
   ) where
 
-import Types
+import Types (Project, EmployeeHours, ProjectHours(ProjectHours))
 
 import Data.Monoid ((<>))
 import Data.Text (Text)
 import qualified Data.Text as T
 import Database.PostgreSQL.Simple
-import Text.InterpolatedString.Perl6 (q)
+import Database.PostgreSQL.Simple.SqlQQ (sql)
+
+project :: Connection -> Int -> IO (Maybe Project)
+project conn pid = do
+  let selectQuery = [sql| select p.id, p.name, c.name
+                          from projects p, customers c
+                          where p.id = ? and p.customer = c.id
+                          order by billable; |]
+  ps <- query conn selectQuery [pid]
+  case ps of
+    [p] -> return (Just p)
+    _   -> return Nothing
 
 projects :: Connection -> IO [Project]
-projects conn = query_ conn [q| select p.id, p.name, c.name
-                                from projects p, customers c
-                                where p.customer = c.id
-                                order by billable; |]
+projects conn = query_ conn [sql| select p.id, p.name, c.name
+                                  from projects p, customers c
+                                  where p.customer = c.id
+                                  order by billable; |]
 
 employeeHours :: Connection -> Int -> Month -> Year -> IO [EmployeeHours]
 employeeHours conn pid month year =
-  let fetchQuery = [q| select e.first_name || ' ' || e.last_name, (
-                       select array_agg(coalesce(t.sum,0))
-                       from generate_series(date ?, date ?, '1 day'::interval) i
-                       left join (select date, sum(minutes) as sum
-                                  from time_entry
-                                  where employee = t.employee
-                                    and project = ?
-                                    and date >= date ?
-                                    and date <= date ?
-                                  group by date ) t on t.date = i::date )
-                       from time_entry t, employees e
-                       where t.employee = e.id
-                       group by t.employee, e.first_name, e.last_name; |]
+  let fetchQuery = [sql| select e.first_name || ' ' || e.last_name, (
+                          select array_agg(coalesce(t.sum,0))
+                          from generate_series(date ?, date ?, '1 day'::interval) i
+                          left join (select date, sum(minutes) as sum
+                                      from time_entry
+                                      where employee = t.employee
+                                        and project = ?
+                                        and date >= date ?
+                                        and date <= date ?
+                                      group by date ) t on t.date = i::date )
+                         from time_entry t, employees e
+                         where t.employee = e.id
+                         group by t.employee, e.first_name, e.last_name; |]
   in query conn fetchQuery (startDate, endDate, pid, startDate, endDate)
   where startDate, endDate :: Text
         startDate = T.pack $ show year <> "-" <> show month <> "-1"
         endDate   = T.pack $ show year <> "-" <> show month <> "-" <> show (lastDay month year)
+
+projectHours :: Connection -> Int -> Month -> Year -> IO ProjectHours
+projectHours conn pid mon year = do
+  (Just p) <- project conn pid
+  hs <- employeeHours conn pid mon year
+  return $ ProjectHours p hs
 
 type Month = Int
 type Year = Int
--- src/Types.hs
+++ src/Types.hs
@@ -8,10 +8,12 @@ module Types
   (
     Project(..),
     EmployeeHours(..),
-    CSV
+    ProjectHours(..),
+    ExcelCSV
   ) where
 
 import Data.Char (ord)
+import Data.Monoid ((<>))
 import GHC.Generics
 import Data.Text (Text)
 import Data.Vector (Vector)
@@ -24,6 +26,7 @@ import Database.PostgreSQL.Simple.FromRow
 import Servant.API.ContentTypes
 import Network.HTTP.Media ((//), (/:))
 
+-- A project
 data Project = Project {
     id :: Int
   , name :: Text
@@ -34,6 +37,10 @@ instance ToRecord Project
 instance FromRow Project
 instance ToJSON Project
 
+instance MimeRender ExcelCSV Project where
+  mimeRender _ (Project _ name' _) = cs name'
+
+-- A list of hours worked for an employee
 data EmployeeHours = EmployeeHours {
     name :: Text
   , minutes :: Vector Int
@@ -44,18 +51,44 @@ instance FromRow EmployeeHours
 
 -- An employee row is the employee name prepended to the list of hours
 instance ToRecord EmployeeHours where
-    toRecord (EmployeeHours name' minutes') = toField name' `V.cons` toRecord hours
-      where hours :: Vector EuDecimal
-            hours = V.map (EuDecimal . (/60.0) . fromIntegral) minutes'
+    toRecord (EmployeeHours name' minutes') = toField name' `V.cons` toRecord hours'
+      where hours' :: Vector EuDecimal
+            hours' = V.map (EuDecimal . (/60.0) . fromIntegral) minutes'
 
-data CSV
+-- A project together with hours worked by employees
+data ProjectHours = ProjectHours {
+    project :: Project
+  , hours :: [EmployeeHours]
+  } deriving (Generic, Show)
 
-instance Accept CSV where
+instance ToJSON ProjectHours
+
+data ExcelCSV
+
+instance Accept ExcelCSV where
   contentType _ = "text" // "csv" /: ("charset", "utf-8")
 
-instance ToRecord a => MimeRender CSV [a] where
+instance ToRecord a => MimeRender ExcelCSV [a] where
   mimeRender _ = encodeWith options
-    where options = defaultEncodeOptions { encDelimiter = fromIntegral (ord '\t') }
+    where options = defaultEncodeOptions {
+                        encDelimiter = fromIntegral (ord '\t')
+                      , encUseCrLf = True
+                      }
+
+-- An Excel-friendly csv file starts with a byte-order mark to indicate that
+-- it's utf-8, then `sep=<tab>` to specify that tab is used as the separator.
+-- Line endings are CRLF.
+instance MimeRender ExcelCSV ProjectHours where
+  mimeRender proxy ph = utf8ByteOrderMark
+                     <> separatorHeader
+                     <> newLine
+                     <> mimeRender proxy (project ph)
+                     <> newLine
+                     <> mimeRender proxy (hours ph)
+    where utf8ByteOrderMark = "\xef\xbb\xbf"
+          separatorHeader = "sep=\t"
+          newLine = "\r\n"
+
 
 -- a data type representing decimals that should use comma as a separator
 newtype EuDecimal = EuDecimal Double
