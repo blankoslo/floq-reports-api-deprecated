190fa516211a55454bc443ee3bb8ba52f9288972
Erlend Hamberg <eh@blankoslo.no>
2016-06-18 12:54:32 +0200

    Remove gcs deployment

--- dist.bash
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-VERSION=$(git describe --always --tags --long | sed -e 's,-,.,')
-
-mkdir -p dist
-stack install --local-bin-path=dist
-mv -v dist/floq-reports dist/floq-reports-${VERSION}
