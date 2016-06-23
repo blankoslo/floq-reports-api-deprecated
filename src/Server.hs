ebdcff7f383687f968f9c7d81cb77c568edf0739
Erlend Hamberg <eh@blankoslo.no>
2016-06-23 23:41:01 +0200

    Allow jwt to be passed in query string

--- src/Server.hs
+++ src/Server.hs
@@ -16,10 +16,10 @@ import qualified Database as DB
 
 import Control.Monad.IO.Class
 import Data.ByteString (ByteString)
+import qualified Data.ByteString as BS
 import Data.Monoid ((<>))
 import Data.String.Conversions (cs)
 import Data.Text (Text)
-import qualified Data.Text as T
 import Database.PostgreSQL.Simple hiding ((:.))
 import Network.Wai
 import Network.Wai.Middleware.Cors
@@ -30,16 +30,18 @@ import Jose.Jwt
 
 validateJwt :: Text -> ByteString -> Handler Jws
 validateJwt jwtSecret authHeader =
-  let jwt = T.drop (T.length "Bearer ") (cs authHeader)
-  in case hmacDecode (cs jwtSecret) (cs jwt) of
+  let jwt = cs authHeader
+  in case hmacDecode (cs jwtSecret) jwt of
         Right verifiedJwt -> return verifiedJwt
         Left err          -> throwError (err403 { errBody = "Invalid signature: " <> (cs . show) err })
 
 authHandler :: Text -> AuthHandler Request Jws
 authHandler jwtSecret =
   let handler req = case lookup "authorization" (requestHeaders req) of
-        Nothing -> throwError (err401 { errBody = "Missing authorization header" })
-        Just authCookieKey -> validateJwt jwtSecret authCookieKey
+        Nothing -> case lookup "jwt" (queryString req) of
+          Just (Just (jwt)) -> validateJwt jwtSecret jwt
+          _                 -> throwError (err401 { errBody = "Missing authorization header" })
+        Just jwt -> validateJwt jwtSecret $ BS.drop (BS.length "Bearer ") jwt
   in mkAuthHandler handler
 
 type ProjectsApi = "projects"
